c35ce95852afe8e8a4751fffe02aea8673a09281
Christian Patzer <cpatze@wirelessadvocates.com>
2018-02-13 16:00:44 -0800

    Committing somewhat final changes

--- AutoCompleteList/ClientApp/app/components/autolist/autolist.component.ts
+++ AutoCompleteList/ClientApp/app/components/autolist/autolist.component.ts
@@ -3,41 +3,92 @@ import { JSONFileService } from '../../services/json.file.service';
 
 @Component({
     selector: 'auto-list',
-    templateUrl: './autolist.component.html',
-    providers: [JSONFileService]
+    templateUrl: './autolist.component.html'
 })
 export class AutoListComponent implements AfterViewInit{
-    public list: ListItem[] = [];
+    public options: ListItem[] = [];
     public searchResults: ListItem[] = [];
     public selectedItem: ListItem = new ListItem();
+    public highlightedIndex: number = 0;
 
     @Input()
     onSelection: EventEmitter<ListItem> = new EventEmitter<ListItem>();
 
-    constructor(@Inject(JSONFileService) private service: JSONFileService) {
-        service.getFile<ListItem[]>("MOCK_DATA_1000.json").then(
-            (value: void | ListItem[]) => {
-                if (value != null) {
-                    this.list = value;
-                }
-                //JSON.parse(value, (key: any, value: any) => {
-                //    var item = new ListItem();
-                //    item.label = key;
-                //    item.value = value;
-                //    this.list.push(item);
-                //});
+    constructor() { }
+
+    private scrollHightlightedIntoView(): void {
+        var highlightedItem = this.searchResults[this.highlightedIndex];
+        var highlightedEle = document.getElementById(highlightedItem.value);
+        var resultsDivEle = document.getElementById("Results");
+        if (highlightedEle != null) {
+            var top = highlightedEle.offsetTop;
+            if (resultsDivEle != null) {
+                resultsDivEle.scrollTop = top;
             }
-        );   
+        }
+
     }
 
     ngAfterViewInit() {
         var ele = document.getElementById("ListSearch");
         if (ele != null) {
             ele.onkeyup = (ev: KeyboardEvent): any => {
-                var searchString = ele == null ? "" : ele.innerText;
-                this.searchResults = this.list.filter((value: ListItem) => {
-                    return value.label.toLowerCase().startsWith(searchString.toLowerCase());
-                });
+                console.log("keyCode: " + ev.keyCode);
+                if (ev.keyCode == 40 && this.searchResults.length > 0 && this.highlightedIndex < this.searchResults.length - 1) {
+                    this.searchResults[this.highlightedIndex].isHighlighted = false;
+                    this.searchResults[++this.highlightedIndex].isHighlighted = true;
+                    this.scrollHightlightedIntoView();
+                    ev.preventDefault();
+                    return false;
+                }
+                else if (ev.keyCode == 38 && this.searchResults.length > 0 && this.highlightedIndex > 0) {
+                    this.searchResults[this.highlightedIndex].isHighlighted = false;
+                    this.searchResults[--this.highlightedIndex].isHighlighted = true;
+                    this.scrollHightlightedIntoView();
+                    ev.preventDefault();
+                    return false;
+                }
+                else if (ev.keyCode == 13 && this.searchResults.length > 0) {
+                    var highlighted = this.searchResults.find((item: ListItem) => {
+                        return item.isHighlighted;
+                    });
+                    if (highlighted != undefined) {
+                        this.selectedItem = highlighted;
+                        this.searchResults = [];
+                        this.onSelection.emit(this.selectedItem);
+                    }
+                    ev.preventDefault();
+                    return false;
+                }
+                else {
+                    var searchString = ele == null ? "" : (<HTMLInputElement>ele).value;
+
+                    //deselect any selected
+                    this.searchResults.forEach((item: ListItem) => {
+                        item.isHighlighted = false;
+                    });
+
+                    if (searchString != null && searchString.length > 0) {
+                        this.searchResults = this.options.filter((value: ListItem) => {
+                            return value.label.toLowerCase().startsWith(searchString.toLowerCase());
+                        });
+
+                        if (this.searchResults.length > 0) {
+                            this.highlightedIndex = 0;
+                            this.searchResults[this.highlightedIndex].isHighlighted = true;
+                        }
+                    }
+                    else if(searchString.length == 0) {
+                        this.searchResults = [];
+                        this.highlightedIndex = 0;
+                        this.selectedItem = new ListItem();
+                        this.onSelection.emit(this.selectedItem);
+                    }
+                }
+
+                console.log("Selected: " + this.selectedItem.label);
+                console.log("Results: " + this.searchResults);
+
             };
         }
     }
@@ -46,19 +97,16 @@ export class AutoListComponent implements AfterViewInit{
         var ele = document.getElementById("ListSearch");
         if (ele != null) {
             this.selectedItem = result;
+            ele.innerHTML = "";
             ele.innerHTML = this.selectedItem.label;
             this.searchResults = [];
         }
         this.onSelection.emit(this.selectedItem);
-    }
-
-
-    public valueChanged(value: any):void {
-        console.log(value);
     }
 }
 
 export class ListItem {
     public label: string;
     public value: string;
+    public isHighlighted: boolean;
 }
--- AutoCompleteList/ClientApp/app/components/home/home.component.ts
+++ AutoCompleteList/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Inject, AfterViewInit } from '@angular/core';
+import { AutoListComponent, ListItem } from '../autolist/autolist.component';
+import { JSONFileService } from '../../services/json.file.service';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    providers: [JSONFileService]
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+
+    @ViewChild(AutoListComponent)
+    private autoList: AutoListComponent;
+    private selectedListItem: ListItem = new ListItem();
+
+
+    constructor(@Inject(JSONFileService) private service: JSONFileService) {
+        service.getFile<ListItem[]>("MOCK_DATA_1000.json").then(
+            (value: void | ListItem[]) => {
+                if (value != null) {
+                    this.autoList.options = value;
+                }
+            }
+        );
+    }
+
+    ngAfterViewInit() {
+        this.autoList.onSelection.subscribe((item: ListItem) => {
+            this.selectedListItem = item;
+        });
+    }
+
 }
